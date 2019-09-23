9f336a80b3957ebba0f7623772e9a5294078b54d
Edison Moreland <edison.moreland@officeally.com>
2019-09-23 09:03:41 -0700

    Renamed package

--- jwt.go
+++ jwt.go
@@ -1,4 +1,4 @@
-package jwt
+package tokenware
 
 // echo.labstack.com/cookbook/jwt
 
--- revocation_test.go
+++ revocation_test.go
@@ -1,15 +1,10 @@
-package jwt
+package tokenware_test
 
 import (
+	"github.com/edison-moreland/tokenware"
 	"testing"
-	"time"
 )
 
-func configureForRevocation(revokedTTL time.Duration) {
-	// Configure only the options this file cares about
-	_pkgconfig = Config{RevokedTimeToLive: 1 * time.Minute}
-	_isconfiginitialized = true
-}
-
 func TestRevoke(t *testing.T) {
+
 }
